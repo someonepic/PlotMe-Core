7f285a92e78fca49889460f9329cd4d4652e20e4
ZachBora <zachbora@gmail.com>
2013-11-13 12:54:52 -0500

    Update DelegateClassException.java
    Removed junk

--- src/main/java/com/worldcretornica/plotme_core/DelegateClassException.java
+++ src/main/java/com/worldcretornica/plotme_core/DelegateClassException.java
@@ -1,22 +1,7 @@
-/*
- * Copyright (C) 2013 Fabrizio Lungo <fab@lungo.co.uk> - All Rights Reserved
- *
- * Unauthorized copying of this file, via any medium is strictly prohibited
- * Proprietary and confidential
- *
- * Created by Fabrizio Lungo <fab@lungo.co.uk>, November 2013
- */
 package com.worldcretornica.plotme_core;
 
-/**
- *
- * @author Fabrizio Lungo <fab@lungo.co.uk>
- */
 public class DelegateClassException extends Exception {
 
-    /**
-	 * 
-	 */
 	private static final long serialVersionUID = 2880074023714953474L;
 
 	public DelegateClassException(Throwable cause) {
